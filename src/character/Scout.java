031d25c0a790b67a8599a096b99ecfc074a1ef6c
Jinnapat Yana <jaja_yana@hotmail.com>
2021-05-19 15:08:34 +0700

    Add running animation

--- src/character/Scout.java
+++ src/character/Scout.java
@@ -1,5 +1,7 @@
 package character;
 
+import java.util.ArrayList;
+
 import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -7,17 +9,39 @@ import javafx.scene.image.ImageView;
 public class Scout extends Character{
 	private AnimationTimer animationLoop;
 	private double lastTriggerTime;
+	private int curImage = 0;
+	private ArrayList<Image> runImageViews;
 	
 	public Scout() {
 		super(50.0, 50.0, 10.0, 10, 100);
 		lastTriggerTime = 0.0;
 		setHealth(100);
 		setMaxHealth(100);
+		runImageViews = new ArrayList<Image>();
+		for (int i = 0; i < 6; i++) {
+			runImageViews.add(new Image(ClassLoader.getSystemResource("character/Black/run/" + (i+1) + ".png").toString()));
+		}
 		
-		String imageUrl = ClassLoader.getSystemResource("character/Black/run/black_run_1.png").toString();
-		imageView = new ImageView(new Image(imageUrl));
+		imageView = new ImageView(runImageViews.get(curImage));
 		imageView.setFitHeight(50.0);
 		imageView.setPreserveRatio(true);
 		getBoundBox().getChildren().add(imageView);
+		
+		this.animationLoop = new AnimationTimer() {
+			
+			@Override
+			public void handle(long now) {
+				
+				lastTriggerTime = (lastTriggerTime < 0 ? now : lastTriggerTime);
+				
+				if (now - lastTriggerTime >= 100000000) {
+					curImage = (curImage + 1) % 6;
+					imageView.setImage(runImageViews.get(curImage));
+					lastTriggerTime = now;
+				}
+			}
+		};
+		
+		this.animationLoop.start();
 	}
 }
