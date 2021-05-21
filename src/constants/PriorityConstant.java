fed45208c88cb21e81c682630d78e5946b22f9c6
Arm Supakorn <arm.supakorn@gmail.com>
2021-05-21 12:53:09 +0700

    Fix SelectcharacterUI

--- src/constants/PriorityConstant.java
+++ src/constants/PriorityConstant.java
@@ -2,5 +2,8 @@ package constants;
 
 public class PriorityConstant {
 	public PriorityConstant() {}
+	public static final int GROUND = 10;
+	public static final int UTILITY = 80;
+	public static final int BULLET = 90;
 	public static final int CHARACTER = 100;
 }
--- src/gui/SelectCharacterUI.java
+++ src/gui/SelectCharacterUI.java
@@ -2,6 +2,7 @@ package gui;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
@@ -10,16 +11,24 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.image.ImageView;
 import logic.ImageLogic;
+
+import java.util.ArrayList;
+
 import character.Character;
+import character.Engineer;
+import character.Heavy;
 import character.Scout;
+import character.Shield;
+import character.Sniper;
 
 public class SelectCharacterUI extends VBox{
 	private Character character;
 	private Image characterImage;
 	private TextField nameInput;
-	private GridPane selectCharacter;
+	private VBox selectCharacter;
 
 	public SelectCharacterUI() {
 		super();
@@ -32,9 +41,9 @@ public class SelectCharacterUI extends VBox{
 		this.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 		
 		this.setCharacter(new Scout());
+		this.setNameInput();
+		this.setSelectCharacter();
 		
-		this.nameInput = new TextField();
-		this.nameInput.setText(this.character.getName());
 		this.checkChange();
 	}
 
@@ -42,13 +51,70 @@ public class SelectCharacterUI extends VBox{
 		this.character = character;
 		this.setCharacterImage(this.character.getSprite());
 	}
+	
+	private void setCharacter(String className) {
+		switch(className) {
+			case "Scout":
+				this.setCharacter(new Scout());
+				break;
+			case "Engineer":
+				this.setCharacter(new Engineer());
+				break;
+			case "Heavy":
+				this.setCharacter(new Heavy());
+				break;
+			case "Shield":
+				this.setCharacter(new Shield());
+				break;
+			case "Sniper":
+				this.setCharacter(new Sniper());
+				break;
+		}
+	}
 
 	public void setCharacterImage(Image characterImage) {
 		this.characterImage = characterImage;
 	}
 	
+	public void setNameInput() {
+		this.nameInput = new TextField();
+		this.nameInput.setAlignment(Pos.CENTER);
+		this.nameInput.setFont(new Font(30));
+		this.nameInput.setText(this.character.getName());
+	}
+
+	public void setSelectCharacter() {
+		this.selectCharacter = new VBox();
+		this.selectCharacter.setAlignment(Pos.CENTER);
+		this.selectCharacter.setPadding(new Insets(20));
+		this.selectCharacter.setSpacing(20);
+		
+		ArrayList<String> characterClass = new ArrayList<String>();
+		characterClass.add("Scout");
+		characterClass.add("Engineer");
+		characterClass.add("Heavy");
+		characterClass.add("Shield");
+		characterClass.add("Sniper");
+		
+		for(int i=0; i<characterClass.size();i++) {
+			
+			String name = characterClass.get(i);
+			Button characterBtn = new Button();
+			characterBtn.setText(name);
+			characterBtn.setPadding(new Insets(0, 20, 0, 20));
+			characterBtn.setFont(new Font(20));
+			
+			characterBtn.setOnMouseClicked((event) -> {
+				System.out.println("Select: " + name);
+				this.setCharacter(name);
+			});
+			this.selectCharacter.getChildren().add(characterBtn);
+		}
+	}
+
 	public void checkChange() {
 		this.getChildren().add(new ImageView(ImageLogic.resizeImage(characterImage, 200, 200)));
 		this.getChildren().add(this.nameInput);
+		this.getChildren().add(this.selectCharacter);
 	}
 }
