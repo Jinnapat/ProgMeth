a09447da4be045f5ea30d752b72bfca8d105d686
Jinnapat Yana <jaja_yana@hotmail.com>
2021-05-21 07:43:20 +0700

    Fix Ground class to use canvas instead

--- src/sceneObject/Ground.java
+++ src/sceneObject/Ground.java
@@ -1,16 +1,12 @@
 package sceneObject;
 
-import java.util.ArrayList;
-
 import character.Character;
 import interfaces.Collidable;
 import item.base.Utility;
-import javafx.geometry.Insets;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import logic.ImageLogic;
+import logic.RenderableHolder;
 
 public class Ground extends SolidObject{
 	
@@ -28,18 +24,7 @@ public class Ground extends SolidObject{
 		image = ImageLogic.resizeImage(image, width, height);
 		this.setSprite(image);
 		
-		int tileNumber = (int) (width / 50);
-		double blockWidth = width / tileNumber;
-		for (int i = 0; i < tileNumber; i++) {
-			ImageView imageView = new ImageView(image);
-			imageView.setFitHeight(height);
-			imageView.setFitWidth(blockWidth);
-			AnchorPane.setLeftAnchor(imageView, i * blockWidth);
-			this.getBoundBox().getChildren().add(imageView);
-		}
-		
-		AnchorPane.setTopAnchor(this.getBoundBox(), getY());
-		AnchorPane.setLeftAnchor(this.getBoundBox(), getX());
+		RenderableHolder.getInstance().addObject(this);
 		GameScene.solidObjects.add(this);
 		GameScene.root.getChildren().add(getBoundBox());
 		checkCollide();
@@ -122,8 +107,6 @@ public class Ground extends SolidObject{
 					targetUtility.setSpeed_y(0.0);
 					targetUtility.setY(this.getY() - targetUtility.getHeight());
 				}
-				
-				
 			}
 
 			System.out.println("yPos: "+targetUtility.getY());
@@ -133,30 +116,25 @@ public class Ground extends SolidObject{
 
 	@Override
 	public int getZ() {
-		// TODO Auto-generated method stub
 		return 10;
 	}
 
 	@Override
 	public void draw(GraphicsContext gc) {
-		// TODO Auto-generated method stub
 		int tileNumber = (int) (this.getWidth() / 50);
 		double blockWidth = this.getWidth() / tileNumber;
 		for (int i = 0; i < tileNumber; i++) {
-//			System.out.println(blockWidth);	
 			gc.drawImage(this.getSprite(), this.getX() + i*blockWidth, this.getY(), blockWidth, this.getHeight());
 		}
 	}
 
 	@Override
 	public boolean isDestroy() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void onCollide(Collidable target) {
-		// TODO Auto-generated method stub
 		if (target instanceof Character) {
 			Character targetCharacter = (Character)target;
 			
