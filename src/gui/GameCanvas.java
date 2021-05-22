1303337ff185b6c20e5caf4a04d56033b4022883
Arm Supakorn <arm.supakorn@gmail.com>
2021-05-23 01:08:32 +0700

    Fix sound

--- src/gui/GameCanvas.java
+++ src/gui/GameCanvas.java
@@ -210,7 +210,7 @@ public class GameCanvas extends Canvas{
 	private void loadResource() {
 		new RenderableHolder();
 		new ImageHolder();
-//		new SoundHolder();
+		new SoundHolder();
 		new FontHolder();
 	}
 	
